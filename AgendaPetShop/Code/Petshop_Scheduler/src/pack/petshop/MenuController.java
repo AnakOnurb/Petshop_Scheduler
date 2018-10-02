79fab07fb1f247b5924b78c5ed1ad30de52f8761
Bruno Castellani <bruno-castellani@hotmail.com>
2018-10-02 23:11:48 +0000

    adição do funcionários e botão de sair

--- AgendaPetShop/Code/Petshop_Scheduler/src/pack/petshop/MenuController.java
+++ AgendaPetShop/Code/Petshop_Scheduler/src/pack/petshop/MenuController.java
@@ -25,63 +25,87 @@ public class MenuController implements Initializable
 	@FXML
 	protected void btnMenuHandler(ActionEvent event)
 	{
+		lblTitle.setText("Menu Inicial");
 		loadContent("inicial.fxml");
 	}
 	
 	@FXML
 	protected void btnClientesHandler(ActionEvent event) 
 	{
+		lblTitle.setText("Cadastro de Cliente");
 		loadContent("cliente.fxml");
 	}
 	
 	@FXML
 	protected void btnDonosHandler(ActionEvent event)
 	{
+		lblTitle.setText("Cadastro de Dono");
 		loadContent("dono.fxml");
 	}
 	
 	@FXML
 	protected void btnAgendaHandler(ActionEvent event)
 	{
+		lblTitle.setText("Agenda");
 		loadContent("agenda.fxml");
 	}
 	
 	@FXML
 	protected void btnAgendamentoHandler(ActionEvent event)
 	{
+		lblTitle.setText("Agendamentos");
 		loadContent("agendamento.fxml");
 	}
 	
 	@FXML
 	protected void btnPacotesHandler(ActionEvent event)
 	{
+		lblTitle.setText("Cadastro de Pacotes");
 		loadContent("pacote.fxml");
 	}
 	
 	@FXML
 	protected void btnPagamentosHandler(ActionEvent event)
 	{
+		lblTitle.setText("Pagamentos");
 		loadContent("pagamento.fxml");
 	}
 	
 	@FXML
 	protected void btnPendenciasHandler(ActionEvent event)
 	{
+		lblTitle.setText("Pendências");
 		loadContent("pendencia.fxml");
 	}
 	
 	@FXML
 	protected void btnConsultaHandler(ActionEvent event)
 	{
+		lblTitle.setText("Consultas");
 		loadContent("consulta.fxml");
+	}
+	
+	@FXML
+	protected void btnFuncionariosHandler(ActionEvent event) 
+	{
+		lblTitle.setText("Funcionários");
+		loadContent("funcionario.fxml");
 	}
 	
 	@FXML
 	protected void btnSobreHandler(ActionEvent event) 
 	{
+		lblTitle.setText("Sobre");
 		loadContent("sobre.fxml");
 	}
 
+	@FXML
+	protected void btnSairHandler(ActionEvent event) 
+	{
+		System.exit(0);
+	}
+
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	{
